046fefef381edd8d2e60ab65416ee57d23a5e99e
glavak <gau-gau@ya.ru>
2016-05-19 13:31:04 +0600

    Progression calculation progs added

---
+++ Programs/arithmetic.asm
@@ -0,0 +1,25 @@
+load 20
+add 21
+save 20
+load 21
+add 22
+save 21
+load 23
+sub 24
+save 23
+goto 0
+load 20
+print
+
+
+
+
+
+
+
+
+const 0 % current summ 
+const 1 % current member
+const 2 % step 
+const 4 % number
+const 1 % step for number
