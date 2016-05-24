e0e7c05e789b17dc22aba97eceae1e7e07fa4783
Glavak <gau-gau@ya.ru>
2016-05-24 20:44:15 +0600

    Some programs added
    Binary/decimal conversion, and arrays

---
+++ Programs/binToDec.asm
@@ -0,0 +1,34 @@
+load 30
+mod 31
+save 44
+load 30
+div 31
+save 30
+load 32
+sub 33
+save 32
+goto 16
+load 40 % ouptup of an array, requries cpu modification to check
+load 41
+load 42
+load 43
+load 44
+print
+load 2
+sub 33
+save 2
+goto 0
+
+
+
+
+
+
+
+
+
+
+const 2042
+const 10
+const 5
+const 1
---
+++ Programs/decToBin.asm
@@ -0,0 +1,35 @@
+load 30
+mod 32
+mul 33
+add 31
+save 31
+load 33
+mul 34
+save 33
+load 30
+div 32
+save 30
+goto 0
+load 31
+print
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const 5
+const 0 % result
+const 2
+const 1 % current power
+const 10
